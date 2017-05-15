60ba829b221ab8b80c4a5eb317c0317f86a2b532
Arj3 <arj3@atalayasec.net>
2017-05-15 18:38:22 +0200

    cache full url instead of domain

--- analysis_handlers/icap_domain.py
+++ analysis_handlers/icap_domain.py
@@ -10,34 +10,33 @@ whitelist = redis.StrictRedis(host=config['redis_host'], port=config['redis_port
 
 def check_domain_quality(url, memory_cache, logger, cache, pass_mode, icap_response):
 
-    domain = deepint.getFQDN(url)
+    #domain = deepint.getFQDN(url)
 
     # checking for domain in whitelist
-    is_whitelisted = whitelist.get(domain)
-    if is_whitelisted:
-        pass
+    #is_whitelisted = whitelist.get(domain)
+    #if is_whitelisted:
+    #    pass
+    # handle in memory cache
+    if url in memory_cache:
+        quality = memory_cache.get(url)
+    # in memory_cache not found, check redis cache
     else:
-        # handle in memory cache
-        if domain in memory_cache:
-            quality = memory_cache.get(domain)
-        # in memory_cache not found, check redis cache
-        else:
-            quality = cache.get(domain)
-        if not quality:
-            quality = deepint.checkDomain(url)
-            cache.setex(domain, config['local_cache_expiration_seconds'], quality)
-            memory_cache[domain] = quality
+        quality = cache.get(url)
+    if not quality:
+        quality = deepint.checkDomain(url)
+        cache.setex(url, config['local_cache_expiration_seconds'], quality)
+        memory_cache[url] = quality
 
-        if quality == 'suspicious':
-            if pass_mode:
-                logger.info('Domain {0} is malicious, pass'.format(domain))
-            else:
-                logger.info('Domain {0} is malicious, block'.format(domain))
-                icap_response.set_icap_response(200)
-                icap_response.set_enc_status('HTTP/1.1 307 Temporary Redirect')
-                icap_response.set_enc_header('location', get_host() + '/malicious-domain?domain=' +
-                                             deepint.getFQDN(url) + '&url=' + url)
-                icap_response.send_headers(False)
-                return True
+    if quality == 'suspicious':
+        if pass_mode:
+            logger.info('Domain {0} is malicious, pass'.format(url))
+        else:
+            logger.info('Domain {0} is malicious, block'.format(url))
+            icap_response.set_icap_response(200)
+            icap_response.set_enc_status('HTTP/1.1 307 Temporary Redirect')
+            icap_response.set_enc_header('location', get_host() + '/malicious-domain?domain=' +
+                                         deepint.getFQDN(url) + '&url=' + url)
+            icap_response.send_headers(False)
+            return True
 
     return False
